1d49126094e64f122a63b96b2aba35222bbe2b04
afrilaknaf <afrilaknaf036@gmail.com>
2026-05-21 00:09:54 +0530

    21-05-26

--- src/Pages/wishlist.jsx
+++ src/Pages/wishlist.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import Loading from "../Compoents/Loading"
 import useGets from "../Hooks/UseGet"
 import Cookies from "js-cookie"
+import wishlist from "../assets/wishlist.png"
 
 export default function Wishlist() {
+
+  const navigate = useNavigate()
 
   const { data, isLoading, isError } = useGets(`wishlist/getdata`)
 
@@ -24,21 +28,81 @@ export default function Wishlist() {
     return item.productId.wacthes === "premium"
   })
 
-  console.log("userid", userid)
-  console.log("normal", normal)
-  console.log("premium", premium)
 
   return (
     <>
-      <h1>Wishlist</h1>
+      <h1 className="px-4 py-5 text-2xl font-bold">Normal</h1>
+      <div className="w-full h-auto flex justify-center items-center flex-wrap gap-5 m-5">
+        {
+          normal.length === 0 ? <>
+            <div className="w-full h-auto  p-5 flex flex-col justify-center items-center">
+              <img src={wishlist} className="w-[300px] md:w-[200px] h-auto" alt="" />
+              <h1 className="text-2xl font-bold text-[#C91F28]">Empty Wishlist </h1>
+            </div>
+          </> : <>
+            {normal?.map((item, index) => (
+              <div
+                key={index}
+                className="w-[300px] group h-[400px] p-4 shadow-2xl transition-all duration-300 border border-gray-200 hover:border-black relative top-0 rounded-2xl mt-3 flex flex-col justify-center items-center"
+              >
+                <img className="w-auto h-auto" src={item.productId.images} alt={item.productId.title} />
 
-      {userid.map((item) => (
-        <div key={item._id}>
-          <h2>{item.productId.title}</h2>
-          <p>{item.productId.price}</p>
-          <img src={item.productId.images?.[0]} width="150" />
-        </div>
-      ))}
+                <h1 className="line-clamp-1 font-bold">
+                  {item.productId.title}
+                </h1>
+
+                <button onClick={() => {
+                  navigate(`/quick/products/${item.productId._id}`)
+                }} className="w-9 h-9 text-xl rounded-full border absolute top-10 start-5 mr-5"><i class="ri-eye-line"></i></button>
+
+                <h1 className="text-[#C91F28] font-bold">₹{item.productId.price}</h1>
+
+                <button className="w-full h-[40px] bg-[#C91F28] hidden group-hover:block text-white rounded mt-3">
+                  Add to Cart
+                </button>
+              </div>
+            ))}
+          </>
+        }
+
+      </div>
+
+      <h1 className="px-4 py-5 text-2xl font-bold">Premium</h1>
+
+
+      <div className="w-full h-auto flex justify-center items-center flex-wrap gap-5 m-5 ">
+        {
+          premium.length === 0 ? <>
+            <div className="w-full h-auto  p-5 flex flex-col justify-center items-center">
+              <img src={wishlist} className="w-[300px] md:w-[200px] h-auto" alt="" />
+              <h1 className="text-2xl font-bold text-[#C91F28]">Empty Wishlist </h1>
+            </div>
+          </> : <>
+          {premium?.map((item, index) => (
+            <div
+              key={index}
+              className="w-[300px] group h-[430px] p-4 shadow-2xl relative top-0 transition-all duration-300 border border-gray-200 hover:border-black rounded-2xl mt-3 justify-center items-center flex flex-col"
+            >
+              <img className="w-[200px] h-[300px]" src={item.productId.thumbnail} alt={item.productId.title} />
+
+              <h1 className="line-clamp-1 font-bold">
+                {item.productId.title}
+              </h1>
+
+              <button onClick={() => navigate(`/quick/${item.productId._id}`)} className="w-9 h-9 text-xl rounded-full border absolute top-10 start-5 mr-5"><i class="ri-eye-line"></i></button>
+
+              <h1 className="text-[#C91F28] font-bold">₹{item.productId.price}</h1>
+
+              <button className="w-full h-[40px] bg-[#C91F28] hidden group-hover:block text-white rounded mt-3">
+                Add to Cart
+              </button>
+            </div>
+            ))}
+          </>
+        }
+      </div>
+
+
     </>
   )
 }
